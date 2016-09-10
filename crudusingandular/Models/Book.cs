0ab9f14e82d0075396d54eb4dfda0d8ce38ca126
GoldPI Developer <info@goldpi.com>
2016-09-10 16:34:12 +0530

    validation

--- crudusingandular/Models/Book.cs
+++ crudusingandular/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +8,15 @@ namespace crudusingandular.Models
 {
     public class Book
     {
+        
         public int  ID{ get; set; }
+        [Required(ErrorMessage = "Plz Enter your Name")]
         public string Title { get; set; }
+        [Required(ErrorMessage ="Plz Enter Your Author Name")]
         public string Author { get; set; }
+        [Required(ErrorMessage ="Plz Enter Your Publisher ")]
         public string publisher { get; set; }
+        [Required(ErrorMessage ="Plz Enter Your ISBN ")]
         public string Isbn { get; set; }
 
     }
